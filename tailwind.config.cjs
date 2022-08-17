9fd94e6cf9802b3f1074652b23d662c23132f71e
Yelban <yelban@gmail.com>
2022-08-17 17:06:09 +0800

    Upgrades to Vite v3.0.8

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,18 @@
 module.exports = {
   content: ['./src/**/*.{html,js,ts,jsx,tsx}', './index.html'],
   theme: {
-    extend: {},
+    extend: {
+      dropShadow: {
+        vite: '0 0 2em #646cffaa',
+        react: '0 0 2em #61dafbaa',
+      },
+      animation: {
+        'spin-slow': 'spin 20s infinite linear',
+      },
+    },
+    fontFamily: {
+      sans: ['Inter', 'Avenir', 'Helvetica', 'Arial', 'sans-serif'],
+    },
   },
   plugins: [],
-}
+};
